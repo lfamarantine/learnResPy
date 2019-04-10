2459236036e3e3b0fe06267c9c2be43e65feff65
dpopadic <dario.popadic@yahoo.com>
2019-04-10 08:17:02 +0100

    pandas I

--- pandas_1.py
+++ pandas_1.py
@@ -3,6 +3,32 @@
 
 # 1. Data ingestion & inspection..
 # --------------------------------
+import pandas as pd
+
+# building data from scratch..
+list_keys = ['Country', 'Total']
+list_values = [['United States', 'Soviet Union', 'United Kingdom'], [1118, 473, 273]]
+# list..
+zipped = list(zip(list_keys,list_values))
+# dictionary..
+data = dict(zipped)
+# dataframe..
+df = pd.DataFrame(data)
+
+# set column names..
+df.columns = ['US', 'RU', 'UK']
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
