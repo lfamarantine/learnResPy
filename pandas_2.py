3aa53eb4a1630a2b9469c8150fe65907c8697bd1
dpopadic <dario.popadic@yahoo.com>
2019-04-15 11:02:04 +0100

    Merge branch 'master' of github.com:dpopadic/datacamp
    # Conflicts:
    #	pandas_1.py

---
+++ pandas_2.py
@@ -0,0 +1,37 @@
+# Manipulating dataframes with pandas
+# -----------------------------------
+
+import pandas as pd
+
+# 1. Extracting and transforming data
+# -----------------------------------
+df = pd.read_csv('data/pd2_sales.csv', index_col='month')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
