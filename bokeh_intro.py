3e95c71a018a706ffc5950f35c395ac9c7009760
dpopadic <dario.popadic@yahoo.com>
2019-06-15 18:36:00 +0100

    adding ml supervised

--- bokeh_intro.py
+++ bokeh_intro.py
@@ -10,13 +10,18 @@
 # - streaming, dynamic, large data
 # - for the browser, with or without a server
 
+import numpy as np
+import pandas as pd
+
 # 1. Plotting with glyphs
 # -----------------------
 # glyphs: visual properties of shapes are called glyphs (circles, squares, triangles, lines,
 # rectangles with properties attached to data)
 from bokeh.io import output_file, show
 from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
 
+help(figure)
 # example use..
 plot = figure(plot_width=400, tools='pan,box_zoom')
 plot.circle([1,2,3,4,5], [8,6,5,2,3])
@@ -40,6 +45,97 @@ show(plot)
 # line(), multi_line()
 # circle(), oval(), ellipse()
 # arc(), quadratic(), bezier()
+
+
+# data formats:
+# numpy arrays..
+x = np.linspace(0, 10, 1000)
+y = np.sin(x) + np.random.random(1000) + 0.2
+plot = figure()
+plot.line(x, y)
+output_file('ex_bokeh.html')
+show(plot)
+
+# column data source..
+# common fundamental data structure for bokeh
+# maps string names to sequences of data
+source = ColumnDataSource(data={
+    'x': [1,2,3,4,5],
+    'y': [8,6,5,2,3]
+})
+source.data
+
+# create CDS directly with pandas..
+from bokeh.sampledata.iris import flowers as df
+df = pd.read_csv('data/bokeh_sprint.csv',sep=',')
+source = ColumnDataSource(df)
+p = figure(x_axis_label='Year', y_axis_label='Time')
+p.circle('Year','Time',source=source,size=8,color='color')
+output_file('ex_bokeh.html')
+show(p)
+
+# customizing glyphs: box-select, hover-tools, color-mapping..
+# ---
+# box_select tool to a figure and change the selected and non-selected circle glyph properties
+# .. box-select:
+p = figure(x_axis_label='Year',y_axis_label='Time',tools='box_select')
+p.circle('Year', 'Time', source=source, selection_color='red', nonselection_alpha=0.1)
+output_file('ex_bokeh.html')
+show(p)
+
+# .. hover-tools:
+from bokeh.models import HoverTool
+p.circle(x, y, size=10,
+         fill_color='grey', alpha=0.1, line_color=None,
+         hover_fill_color='firebrick', hover_alpha=0.5,
+         hover_line_color='white')
+hover = HoverTool(tooltips=None,mode='vline')
+p.add_tools(hover)
+output_file('ex_bokeh.html')
+show(p)
+
+# color-mapping:
+from bokeh.models import CategoricalColorMapper
+df = pd.read_csv('data/bokeh_auto-mpg.csv',sep=',')
+source = ColumnDataSource(df)
+# make a CategoricalColorMapper object..
+color_mapper = CategoricalColorMapper(factors=['Europe', 'Asia', 'US'],
+                                      palette=['red', 'green', 'blue'])
+# add a circle glyph to the figure p..
+p.circle('weight', 'mpg', source=source,
+            color=dict(field='origin', transform=color_mapper),
+            legend='origin')
+output_file('ex_bokeh.html')
+show(p)
+
+
+# 2. Layouts, Interactions & Annotations
+# --------------------------------------
+df = pd.read_csv('data/bokeh_literacy_birth_rate.csv',sep=',')
+source2 = ColumnDataSource(df)
+
+# creating rows of plots..
+from bokeh.layouts import row
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+p1.circle('fertility', 'female literacy', source=source2)
+p2 = figure(x_axis_label='population', y_axis_label='female_literacy (% population)')
+p2.circle('population', 'female literacy', source=source2)
+layout = row(p1,p2)
+output_file('ex_bokeh2.html')
+show(layout)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ ml_sup_learn.py
@@ -0,0 +1,39 @@
+# Supervised Learning with scikit-Learn
+# -------------------------------------
+# other popular ML libraries: TensorFlow, keras, scikit-learn integrates well with SciPy stack
+
+# 1. Classification
+# -----------------
+
+# ML: the art & science of: giving computers the ability to learn to make decisions from data
+# without being explicitly programmed
+# examples: learning to predict whether email is spam, clustering wikipedia entries into different categories
+
+# types of ML:
+# ---
+# supervised: use labeled data
+# unsupervised learning: uses unlabeled data
+# reinforcement learning: software agents interact with an environment (automatically learn how t
+# o optimize their behaviour given a system of rewards & punishments), draws inspiration from behavioural
+# psychology (applications in economics, genetics, game playing), reinforcement learning was used in alphaGo
+
+# supervised learning:
+# - predictor variables/features and a target variable
+# - 2 categories: classification (target variable consists of categories) or regression (target variable is continuous)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
