9ba1e8afb105a42af7652b7c5719307ee734f458
dpopadic <dario.popadic@yahoo.com>
2019-06-18 23:40:05 +0100

    update on ml unsupervised

--- ml_unsup_learn.py
+++ ml_unsup_learn.py
@@ -2,13 +2,33 @@
 # -------------------------------
 
 
+# 1. Clustering for dataset exploration
+# -------------------------------------
+# Unsupervised learning encompasses a variety of techniques in machine learning, from clustering to
+# dimension reduction to matrix factorization
+# idea: classify stocks to outperform / underperform and then perform ML techniques instead of traditional ic-analysis
+
+# k-Means..
+# ---
 
 
+# measuring clustering quality..
+# ---
+# - insertia measures cluster quality: measures how spread out the clusters are (lower is better)
+# - distance from each sample to centroid of its cluster
+# - choose an "ellbow" in the inertia plot (where inertia begins to decrease more slowly)
 
 
+# 2. Visualization with hierarchical clustering and t-SNE
+# -------------------------------------------------------
 
 
+# 3. Decorrelating your data and dimension reduction
+# --------------------------------------------------
 
+
+# 4. Discovering interpretable features
+# -------------------------------------
 
 
 
