41bf4c4eddd00299053e866e65906fc7fa6e0732
dpopadic <dario.popadic@yahoo.com>
2019-06-25 23:46:14 +0100

    update on ml deep learning

--- ml_deep_learn.py
+++ ml_deep_learn.py
@@ -1,15 +1,25 @@
 # Deep Learning in Python
 # -----------------------
+# deep learning libraries: 1) keras & tensorflow
+# foundations for advanced applications to image, text, audio, video & more
+# - neural networks account for interactions really well
+# - deep learning uses especially powerful for neural networks
 
 # 1. Basics of deep learning and neural networks
+# ----------------------------------------------
+# forward propagation: how neural networks use data to make predictions
+
 
 # 2. Optimizing a neural network with backward propagation
+# --------------------------------------------------------
+
 
 # 3. Building deep learning models with keras
+# ----------------------------------------------
 
 
 # 4. Fine-tuning keras models
-
+# ----------------------------------------------
 
 
 
