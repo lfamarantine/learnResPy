183595fe3173ac89de588c3b904a7eff8c1fce37
dpopadic <dario.popadic@yahoo.com>
2019-06-25 12:54:56 +0100

    update on ml deep learning

---
+++ ml_deep_learn.py
@@ -0,0 +1,16 @@
+# Deep Learning in Python
+# -----------------------
+
+# 1. Basics of deep learning and neural networks
+
+# 2. Optimizing a neural network with backward propagation
+
+# 3. Building deep learning models with keras
+
+
+# 4. Fine-tuning keras models
+
+
+
+
+
---
+++ ml_kaggle_comp.py
@@ -0,0 +1,5 @@
+# Kaggle Competition Case Study
+# -----------------------------
+
+
+
