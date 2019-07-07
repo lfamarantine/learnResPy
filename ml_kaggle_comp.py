cab02403aec9eeb7d16e50fe82a3289356dff946
dpopadic <dario.popadic@yahoo.com>
2019-07-07 19:49:12 +0100

    update on ml kaggle competition example

--- ml_kaggle_comp.py
+++ ml_kaggle_comp.py
@@ -1,6 +1,27 @@
 # Kaggle Competition Case Study
 # -----------------------------
+# the challenge: Budgets for schools are huge, complex & not standardized. Hundreds of hours
+# are spent each year manually labelling them.
+# the goal: build a ml algorithm to automate this process
+# example line item is; "Algebra books for 8th grade students" with labels: math,
+#                        textbooks, middle-school (target variable)
+# supervised classification problem!
 
+
+# 1. Exploring the raw data
+# -------------------------
+
+
+# 2. Creating a simple first model
+# --------------------------------
+
+
+# 3. Improving your model
+# -----------------------
+
+
+# 4. Learning from the experts
+# ----------------------------
 
 
 
