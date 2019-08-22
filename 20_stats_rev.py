d1cf951de77d77587b956611a8473e5daaf9f6e2
dpopadic <dario.popadic@yahoo.com>
2019-08-22 08:55:10 +0100

    stats revision work

--- 20_stats_rev.py
+++ 20_stats_rev.py
@@ -27,10 +27,71 @@
 # prob(faulty | tails):  0.5 / 0.75
 
 
+# quick recap on list comprehensions
+# ---
+# conventional for loop..
+x = [1,2,3,4]
+out = []
+for i in x:
+    out.append(i**2)
+print(out)
+
+# list comprehension..
+out = [i**2 for i in x]
+print(out)
+
+
+# law of large numbers
+# ---
+import numpy as np
+from numpy.random import randint
+# roll a dice 10 & 1000 times..
+small = randint(1, 7, 10)
+small_mean = np.mean(small)
+print(small_mean)
+large = randint(1, 7, 1000)
+large_mean = np.mean(large)
+print(large_mean)
+
+
 # central limit theorem
+# ---
+import matplotlib.pyplot as plt
+
+# a list of 1000 sample means of size 30..
+means = [randint(1, 7, 30).mean() for i in range(1000)]
+plt.hist(means)
+
+
+# probability distributions
+# ---
+from scipy.stats import bernoulli, binom
+
+
+# bernoulli distribution
+# ---
+# .. 2 possible outcomes
+# example: coin flip
+plt.hist(bernoulli.rvs(p=0.5, size=1000))
+
+# binomial distribution
+# ---
+# sum of outcomes of bernoulli trials
+# binomial distribution is used to model the number of successful outcomes in trials
+# where there is some consistent probability of success
+plt.hist(binom.rvs(n=2, p=0.5, size=1000)) # inputs: k, p, n
+
+# normal distribution
 # ---
 
 
+# poission distribution
+# ---
+# represents a count / number of times something has happened
+# parameter lambda
+# example: in a 15-min time interval, there is a 20% probability that
+# you will see at least one shooting star. What's the probability that
+# you see at least one shooting star in the period of an hour?
 
 
 
